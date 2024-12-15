750284010cae8956035572138ca9df95d1b0e0bf
XIONG Zhipeng <zhipeng@akane.waseda.jp>
2024-12-15 18:42:45 +0900

    update

--- LearnGoWithTests/ch1/hello.go
+++ LearnGoWithTests/ch1/hello.go
@@ -1,7 +1,18 @@
+// package main
+
+// import "fmt"
+
+//	func main() {
+//		fmt.Println("Hello, world")
+//	}
 package main
 
 import "fmt"
 
+func Hello() string {
+	return "Hello, world"
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello())
 }
